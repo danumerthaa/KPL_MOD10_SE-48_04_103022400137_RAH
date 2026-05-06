5b50412b9acf6efc4a058e989a935d696aec43a9
HCarrr <huda012354@gmail.com>
2026-05-06 08:02:19 +0700

    commit message

---
+++ KPL_MOD10_SE-48_04_103022400137_RAH/Controllers/Controllers.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace KPL_MOD10_SE_48_04_103022400137_RAH.Controllers
+{
+
+    
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Controllers : ControllerBase
+    {
+        private static List<Games> _games = new List<Games>()
+        {
+            new Games
+            {
+             id = 1,
+             Nama = "Valorant",
+             Developer = "Riot Games",
+             TahunRilis =  2020,
+             Genre = "FPS",
+             Rating = 8.5,
+             Platform = ["PC"],
+             Mode = ["Multiplayer"],
+             isOnline = true,
+             Harga = 0
+            },
+
+
+            new Games
+            {
+             id = 2,
+             Nama = "GTA V",
+             Developer = "Rockstar Games",
+             TahunRilis = 2013,
+             Genre = "Open World",
+             Rating = 9.5,
+             Platform = ["PC","PS4","PS5","XBOX"],
+             Mode = ["Singleplayer"],
+             isOnline = true,
+             Harga = 300000
+            },
+
+
+            new Games
+            {
+             id = 3,
+             Nama = "The Witcher 3",
+             Developer = "CD Projekt Red",
+             TahunRilis = 2015,
+             Genre = "RPG",
+             Rating = 9.7,
+             Platform = ["PC","PS4","PS5","XBOX","Switch"],
+             Mode = ["Singleplayer"],
+             isOnline = false,
+             Harga = 250000
+            },
+        };
+
+        [HttpGet]
+
+        public IEnumerable<Games> Get()
+        {
+            return _games;
+        }
+
+        [HttpGet("{id}")]
+        public Games Get(int id)
+        {
+            return _games[id];
+        }
+
+        [HttpPost]
+        public void Post(Games game)
+        {
+            _games.Add(game);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id,Games update)
+        {
+            var index = _games.FindIndex(g =>  g.id == id);
+            if (index == -1)
+            {
+                return NotFound();
+            }
+            _games[index] = update; 
+            return Ok(update);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _games.RemoveAt(id);
+        }
+    }
+}
---
+++ KPL_MOD10_SE-48_04_103022400137_RAH/Games.cs
@@ -0,0 +1,16 @@
+﻿namespace KPL_MOD10_SE_48_04_103022400137_RAH
+{
+    public class Games
+    {
+        public int id {  get; set; }
+        public string Nama { get; set; }
+        public string Developer { get; set; }
+        public int TahunRilis { get; set; }
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+        public List<string> Platform { get; set; }
+        public List<string> Mode { get; set; }
+        public Boolean isOnline { get; set; }
+        public int Harga { get; set; }
+     }
+}
